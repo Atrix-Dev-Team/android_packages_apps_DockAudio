fd57bfbe844ebebbc7ae1b9545d82bbfb325b3cd
Emilio López <turl@tuxfamily.org>
2011-10-31 22:33:41 +0000

    Add some missing magic

--- src/com/android/DockAudio.java
+++ src/com/android/DockAudio.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.AudioSystem;
 import android.util.Log;
 
 public class DockAudio extends BroadcastReceiver {
@@ -18,9 +19,11 @@ public class DockAudio extends BroadcastReceiver {
             int state = intent.getExtras().getInt("android.intent.extra.DOCK_STATE",1);
             if(state == 0){
                 Log.i(LOG_TAG, "Removed from dock!");
+                AudioSystem.setDeviceConnectionState(0x400, 0x00, "");
                 am.setParameters("DockState=0");
             } else {
                 Log.i(LOG_TAG, "Docked!");
+                AudioSystem.setDeviceConnectionState(0x400, 0x01, ""); //0x1000 HDMI?
                 am.setParameters("DockState="+state);
             }
         }
